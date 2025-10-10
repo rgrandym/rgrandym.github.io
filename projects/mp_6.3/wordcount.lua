18f60f4024cca8327b3bdbe7bf4aff69616c83c0
rgrandy <rodrigograndy@gmail.com>
2025-10-10 11:07:16 +0100

    chore: Move project source files to temp_documents and update .gitignore
    - Moved course notebooks, PDFs, and data files to assets/temp_documents/
    - Added temp_documents/ to .gitignore to exclude from version control
    - These are working files not needed in the public repository

--- projects/mp_6.3/wordcount.lua
+++
@@ -1,9 +0,0 @@
-function CodeBlock() return {} end  -- Ignore code blocks
-function Code() return {} end       -- Ignore inline code
-
-function Doc(body, meta)
-    local str = pandoc.utils.stringify(body)
-    local wordcount = #pandoc.utils.words(str)
-    print("Word count (excluding code): " .. wordcount)
-    return nil
-end
